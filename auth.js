1ec878b91b94da49a8c6d986f0b919607783ce12
Shaganashree97 <shaganashree.pl@gmail.com>
2024-05-23 23:24:05 +0530

    aligning the files

--- auth.js
+++
@@ -1,7 +0,0 @@
-// auth.js
-
-// Check if the user is logged in
-if (!localStorage.getItem("loggedIn")) {
-    // If not logged in, redirect to the login page
-    window.location.href = "index.html?logout=forced";
-}
--- logout.js
+++
@@ -1,4 +0,0 @@
-const logoutbtn = document.querySelector(".logout-btn")
-logoutbtn.addEventListener("click",()=>{
-    window.location.replace("index.html")
-})
